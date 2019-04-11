53377dbdfd159cfd3a01f27735d8cff2c7b4b1fc
PaulaTorresS <paula.torres.salazar@gmail.com>
2019-04-11 12:14:14 -0400

    cifrado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 document.getElementById("botonCifrar").addEventListener("click", function(){  //evento que hara que pueda darse el click
   let palabra = document.getElementById("intext").value; //variable para atraer el valor de el texto que se ingrese
   let offsetnum = parseInt(document.getElementById("offset").value); // variable para el numero del offset que se ingresa
-  let resultadofinal=cipher.encode(palabra,offsetnum)
+  let resultadofinal=cipher.encode(palabra,offsetnum);
   document.getElementById("resultado").value=resultadofinal;
 });
 
@@ -10,7 +10,7 @@ document.getElementById("botonCifrar").addEventListener("click", function(){  //
 document.getElementById("botonDescifrar").addEventListener("click", function(){
   let palabra = document.getElementById("intext").value;
   let offsetnum = parseInt(document.getElementById("offset").value);
-  let resultadofinal=cipher.decode(palabra,offsetnum)
+  let resultadofinal=cipher.decode(palabra,offsetnum);
   //console.console.log(offset);
 document.getElementById("resultado").value=resultadofinal;
 })
