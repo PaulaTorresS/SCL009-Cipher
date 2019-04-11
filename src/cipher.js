183a7c1f9bd52fe5ce98182a2609413c651ddaf3
PaulaTorresS <paula.torres.salazar@gmail.com>
2019-04-11 12:46:24 -0400

    mas

--- src/cipher.js
+++ src/cipher.js
@@ -25,18 +25,19 @@ window.cipher = {
       let resultadofinal = "";
       let resultadoFormula = "";
       //console.console.log(offset);
+
       //para poder Descifrar
       for (let i = 0; i < palabra.length; i++) {
         let ascii = palabra.charCodeAt(i);
         //console.log(ascii);
 
         if (ascii >= 65 && ascii <= 90) {
-          resultadoFormula = String.fromCharCode((ascii + 65 - offsetnum) % 26 + 65);
+          resultadoFormula = String.fromCharCode((ascii - 90 - offsetnum) % 26 + 90);
           resultadofinal += resultadoFormula;
         }
         
         else if (ascii >= 97 && ascii <= 122) {
-          resultadoFormula = String.fromCharCode((ascii + 97 - offsetnum) % 26 + 97);
+          resultadoFormula = String.fromCharCode((ascii - 122 - offsetnum) % 26 + 122);
 
         resultadofinal += resultadoFormula;
       }
